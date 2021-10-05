28b76d47c89b058d5cdda9629ff1bf92bb3d2c92
MarioNedic <mario.nedic@fpmoz.sum.ba>
2021-10-05 14:58:10 +0200

    Wireframe, router, ureden wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Galerija</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-navigation-drawer app v-model="drawer" absolute temporary dark>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-row>
+              <v-col cols="1">
+                <v-btn
+                  v-for="link in links"
+                  :key="link.text"
+                  text
+                  :to="link.path"
+                >
+                  <v-icon>mdi-circle </v-icon>
+                  {{ link.text }}
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
+  data: () => ({
+    drawer: false,
+    group: null,
+    links: [
+      {
+        text: "Home",
+        path: "/",
+      },
+      {
+        text: "PretraziGaleriju",
+        path: "/pretraziGaleriju",
+      },
+      {
+        text: "Login",
+        path: "/login",
+      },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    ],
+  }),
+};
+</script>
